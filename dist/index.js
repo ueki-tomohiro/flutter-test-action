ecda651d8a6fe99198722e36055caa198cc7be83
植木 友浩 <ueki_tomohiro@cyberagent.co.jp>
2021-10-29 00:01:15 +0900

    fix buffer

--- dist/index.js
+++ dist/index.js
@@ -50,46 +50,44 @@ const action_1 = __nccwpck_require__(1231);
 const charactersLimit = 65535;
 const exportReport = (report) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        if (core.getInput('token')) {
-            const octokit = new action_1.Octokit();
-            const owner = github.context.repo.owner;
-            const repo = github.context.repo.repo;
-            const pr = github.context.payload.pull_request;
-            const sha = (pr && pr.head.sha) || github.context.sha;
-            let title = core.getInput('title');
-            if (title.length > charactersLimit) {
-                core.error(`The 'title' will be truncated because the character limit (${charactersLimit}) exceeded.`);
-                title = title.substring(0, charactersLimit);
-            }
-            let summary = report.summary;
-            if (summary.length > charactersLimit) {
-                core.error(`The 'summary' will be truncated because the character limit (${charactersLimit}) exceeded.`);
-                summary = summary.substring(0, charactersLimit);
-            }
-            let reportDetail = report.detail;
-            if (reportDetail.length > charactersLimit) {
-                core.error(`The 'text' will be truncated because the character limit (${charactersLimit}) exceeded.`);
-                reportDetail = reportDetail.substring(0, charactersLimit);
-            }
-            if (report.annotations.length > 50) {
-                core.error('Annotations that exceed the limit (50) will be truncated.');
-            }
-            const annotations = report.annotations.slice(0, 50);
-            yield octokit.checks.create({
-                owner,
-                repo,
-                name: title,
-                head_sha: sha,
-                status: 'completed',
-                conclusion: report.status,
-                output: {
-                    title,
-                    summary,
-                    text: reportDetail,
-                    annotations
-                }
-            });
+        const octokit = new action_1.Octokit();
+        const owner = github.context.repo.owner;
+        const repo = github.context.repo.repo;
+        const pr = github.context.payload.pull_request;
+        const sha = (pr && pr.head.sha) || github.context.sha;
+        let title = core.getInput('title');
+        if (title.length > charactersLimit) {
+            core.error(`The 'title' will be truncated because the character limit (${charactersLimit}) exceeded.`);
+            title = title.substring(0, charactersLimit);
         }
+        let summary = report.summary;
+        if (summary.length > charactersLimit) {
+            core.error(`The 'summary' will be truncated because the character limit (${charactersLimit}) exceeded.`);
+            summary = summary.substring(0, charactersLimit);
+        }
+        let reportDetail = report.detail;
+        if (reportDetail.length > charactersLimit) {
+            core.error(`The 'text' will be truncated because the character limit (${charactersLimit}) exceeded.`);
+            reportDetail = reportDetail.substring(0, charactersLimit);
+        }
+        if (report.annotations.length > 50) {
+            core.error('Annotations that exceed the limit (50) will be truncated.');
+        }
+        const annotations = report.annotations.slice(0, 50);
+        yield octokit.checks.create({
+            owner,
+            repo,
+            name: title,
+            head_sha: sha,
+            status: 'completed',
+            conclusion: report.status,
+            output: {
+                title,
+                summary,
+                text: reportDetail,
+                annotations
+            }
+        });
     }
     catch (error) {
         core.setFailed(error.message);
